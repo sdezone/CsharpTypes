1666d1a81f8186b32a2a5b09d9f861ea33783464
sdezone <mh.nusretkaya@hotmail.com>
2023-08-20 23:27:54 +0300

    ref #103 added code.

--- CsharpTypes/HRM/Employee.cs
+++ CsharpTypes/HRM/Employee.cs
@@ -1,7 +1,15 @@
 class Employee
 {
-    int montlyWage=1234;
-    int months=12, bonus=1000;
-    bool isActive=true;
-    double rating=99.25;
+    int montlyWage = 1234;
+    int months = 12, bonus = 1000;
+    bool isActive = true;
+    double rating = 99.25;
+    public double ratePerHour = 12.34;
+    int numberOfHoursWorked = 165;
+
+    public Employee()
+    {
+        double currentMonthWage = ratePerHour * numberOfHoursWorked;
+
+    }
 }
