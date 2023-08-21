020cb217fbf4957b57970b14738c097f4a1f95b9
sdezone <mh.nusretkaya@hotmail.com>
2023-08-21 17:33:05 +0300

    refs #104 cleared code

--- CsharpTypes/Program.cs
+++ CsharpTypes/Program.cs
@@ -1,5 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Employee employee=new Employee();
+﻿Employee employee=new Employee();
 System.Console.WriteLine(employee.ratePerHour);
